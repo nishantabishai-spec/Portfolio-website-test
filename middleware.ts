2d2511c0542df6208c69bd22f70185df25390d91
nishantabishai-spec <nishantabishai@gmail.com>
2026-04-26 17:59:01 +0530

    sitedown for now index change

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware() {
-  return new Response('Offline for now, might be back later :)', { status: 503 })
-}
-
-export const config = {
-  matcher: '/((?!_next|favicon.ico).*)',
-}
